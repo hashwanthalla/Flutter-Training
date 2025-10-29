cee5dee3d90237c6f750797e5385e850aefc4110
hashwanthalla <hashwanthalla50007@gmail.com>
2025-10-29 23:46:57 +0530

    DART: variables and method and ifelse

--- variablesAndMethods.dart
+++ variablesAndMethods.dart
@@ -3,13 +3,25 @@ void main() {
   double x = 66.66;
   String z= "hashwanth";
   var b = 666.88;
-  mymarks(i);
+  bool ispass;
+  ispass = mymarks(i);
+  print(ispass);
+  if(ispass){
+    print("Send him chocolates");
+  } else
+  {
+    print("Send him books");
+  }
+  
 }
+
 mymarks(int marks) {
   print(marks);
   if(marks>=60){
-    print("well done");
+    print("well done!");
+    return true;
   } else{
-    print("you are doing geat");
+    print("you are doing geat!");
+    return false;
   }
 }
